6586a7bd3e2e68c6a95edd5ba401ebd9edf68fcb
VAno <ivanrumancev02@gmail.com>
2024-09-18 23:19:59 +0300

    Use proper grammar.

--- main.c
+++ main.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(int argc, char **argv)
 {
-    char name[255];
-
-    printf("Enter your name: ");
-    fgets(name, 255, stdin);
-    printf("length = %d\n", strlen(name)); /* debug line */
-
-    printf("Hello %s!\n", name);
+    printf("Hello world!\n");
     return 0;
 }
